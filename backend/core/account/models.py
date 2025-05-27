2f53a70ee6fdcc6b08ed2b5898af31e54d65d1d7
Slant14 <mahmood.naderi98@gmail.com>
2025-05-27 14:54:51 +0300

    feat(accounts/models): add registration of new superuser

--- backend/core/account/models.py
+++ backend/core/account/models.py
@@ -1,14 +1,44 @@
 from django.db import models
-from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, PermissionMixin)
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.base_user import BaseUserManager
 
-class User(AbstractBaseUser, PermissionMixin):
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('Email is not provided!'))
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must be set as staff'))
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must be set as superuser'))
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length = 255, unique = True)
     is_staff = models.BooleanField(default = False)
     is_active = models.BooleanField(default = True)
+    is_superuser = models.BooleanField(default = False)
     created_date = models.DateTimeField(auto_now_add = True)
     updated_date = models.DateTimeField(auto_now = True)
-
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
