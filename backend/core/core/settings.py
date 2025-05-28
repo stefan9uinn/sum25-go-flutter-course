e57fb77a3e4d7932bf468d4c5ae1e5d30fea8adb
Slant14 <mahmood.naderi98@gmail.com>
2025-05-28 21:44:39 +0300

    feat(settings, urls): configure the media & static url

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -117,7 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
