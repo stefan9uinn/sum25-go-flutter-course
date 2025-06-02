e70bdec93d3a5fa04ea04d410a01b40743f49c89
Slan14 <mahmood.naderi98@gmail.com>
2025-06-02 23:05:24 +0300

    feat(class/models): add classroom model and migrations

--- backend/core/class/models.py
+++ backend/core/class/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from account.models import Profile 
 
-# Create your models here.
+class Classroom(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    teacher = models.ForeignKey(to = Profile, on_delete = models.CASCADE)
+
+
+    def __str__(self) -> str:
+        return self.title
