e1e7031b6e52c3fadafff9581ac01a1a2da87f96
Slan14 <mahmood.naderi98@gmail.com>
2025-06-09 11:14:01 +0300

    refactor(class/models): add new fields to class model & register models on admin & add topic model

--- backend/core/class/admin.py
+++ backend/core/class/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Topic, Classroom
 
-# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Classroom)
--- backend/core/class/models.py
+++ backend/core/class/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from account.models import Profile 
 
+class Topic(models.Model):
+    title = models.CharField(max_length = 50)
+    
+    def __str__(self) -> str:
+        return self.title
+
 class Classroom(models.Model):
     title = models.CharField(max_length = 200)
     description = models.TextField()
     teacher = models.ForeignKey(to = Profile, on_delete = models.CASCADE)
-
+    topic = models.ForeignKey(to = Topic, on_delete = models.DO_NOTHING, null = True)
+    created_date = models.DateTimeField(auto_now_add = True)
+    capacity = models.IntegerField()
 
     def __str__(self) -> str:
         return self.title
