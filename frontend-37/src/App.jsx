409e78f5a8fbb8eea8567e16ee58978b80a007ac
ustkost <q2zoff.jr@gmail.com>
2025-06-13 18:19:00 +0300

    build(deploy.yml, App): verify CI/CD is working

--- frontend-37/src/App.jsx
+++ frontend-37/src/App.jsx
@@ -1,7 +1,15 @@
 import { Button } from "./components/ui/Button";
 
 export default function App() {
-  return <div>
-    <Button style={{width: 100}} onClick={() => alert("salam bro")}>hello bro</Button>
-  </div>
+  return (
+    <div>
+      <Button
+        style={{ width: 100 }}
+        onClick={() => alert("salam bro real good")}
+      >
+        hello bro check out my ci/cd
+      </Button>
+    </div>
+  );
 }
+
