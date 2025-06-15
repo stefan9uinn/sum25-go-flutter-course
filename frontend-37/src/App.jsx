dac0f8b17901e52165f7bba261b98001e281fba0
err0r522 <47586934+err0r522@users.noreply.github.com>
2025-06-15 20:57:59 +0300

    fix(App.jsx): add topbar to the main page

--- frontend-37/src/App.jsx
+++ frontend-37/src/App.jsx
@@ -1,7 +1,14 @@
 import { Button } from "./components/ui/Button";
+import { MainBar } from "./components/ui/MainBar";
 
 export default function App() {
-  return <div>
-    <Button style={{width: 100}} onClick={() => alert("salam bro")}>hello bro</Button>
-  </div>
+  return <>
+  <div>
+      <MainBar></MainBar>
+
+      <Button style={{ width: 200 }} onClick={() => alert("salam bro")}>
+        hello bro (ci/cd test)
+      </Button>
+    </div>
+  </>
 }
