a7c3108f8e1ebacb3eb075556f91be884a921606
err0r522 <47586934+err0r522@users.noreply.github.com>
2025-06-15 20:57:07 +0300

    feat(MainBar.jsx, PlaygroundBar.jsx, TemplateChoiceBar.jsx): create topbar variations for different pages

---
+++ frontend-37/src/components/ui/MainBar.jsx
@@ -0,0 +1,15 @@
+import { TopBar } from "./TopBar";
+import { TopBarElement } from "./TopBarElement";
+import { Button } from "./Button";
+
+export function MainBar(){
+    return <TopBar>
+        <TopBarElement><a href="/">Main</a></TopBarElement>
+        <TopBarElement><a href="/about">About</a></TopBarElement>
+        <TopBarElement>
+            <a href="/playground">
+                <Button style={{width: 223.93 + "px", height: 51.15 + "px", borderRadius: 11 + "px"}}>Playground</Button>
+            </a>
+        </TopBarElement>
+    </TopBar>
+}
