564860bdf859ec03ea88fb5c88d97707206cd44a
ustkost <q2zoff.jr@gmail.com>
2025-06-18 22:10:21 +0300

    feat(TableSchema, index.css, Playground): add TableSchema component

--- frontend-37/src/pages/Playground.jsx
+++ frontend-37/src/pages/Playground.jsx
@@ -1,10 +1,34 @@
 import { PlaygroundBar } from "../components/PlaygroundBar";
+import TableSchema from "../components/TableSchema";
 
 export default function Playground() {
+  const schema = {
+    name: "test",
+    columns: [
+      {
+        name: "id",
+        type: "SERIAL",
+        attrs: "PRIMARY KEY",
+      },
+      {
+        name: "name",
+        type: "VARCHAR(30)",
+        attrs: "NOT NULL",
+      },
+      {
+        name: "age",
+        type: "INTEGER",
+        attrs: "DEFAULT 18",
+      },
+    ],
+  };
+
   return (
     <div>
       <PlaygroundBar />
-      playground page
+      <div className="mono">
+        <TableSchema schema={schema} />
+      </div>
     </div>
   );
 }
