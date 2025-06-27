895edac98f91384471df00eb77f29936f30c6627
StefFashka <s.muravev@innopolis.university>
2025-06-27 20:42:22 +0300

    My local changes1

--- backend/core/backend_db/admin.py
+++ backend/core/backend_db/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Classroom, Enrollment, Course, Assignment, Submission
+from django.conf import settings
 
-# Register your models here.
+@admin.register(Classroom)
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at')
+    search_fields = ()
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'classroom', 'grade', 'enrollment_date')
+    list_filter = ('classroom',)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    search_fields = ('title',)
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course', 'created_at')
+    list_filter = ('course',)
+    search_fields = ('name',)
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('student', 'assignment', 'created_at')
+    list_filter = ('assignment',)
