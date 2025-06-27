7fd758c7f59ddedac80e2cb59e4ff2177565a980
StefFashka <s.muravev@innopolis.university>
2025-06-27 20:46:51 +0300

    My local changes2

---
+++ backend/core/backend_db/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Classroom, Enrollment, Course, Assignment, Submission
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class ClassroomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Classroom
+        fields = '__all__'
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enrollment
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = '__all__'
+
+class SubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Submission
+        fields = '__all__' 
