2ac219edbd3d68ebe44208a702e164fe778ced2e
StefFashka <s.muravev@innopolis.university>
2025-06-27 20:44:53 +0300

    My local changes2

--- backend/core/backend_db/models.py
+++ backend/core/backend_db/models.py
@@ -1,3 +1,51 @@
+# backend/school/models.py
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Classroom(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Classroom {self.id}"
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='enrollments')
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, related_name='enrollments')
+    grade = models.FloatField(null=True, blank=True)
+    enrollment_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('student', 'classroom')
+
+    def __str__(self):
+        return f"{self.student} in {self.classroom}"
+
+class Course(models.Model):
+    classrooms = models.ManyToManyField(Classroom, related_name='courses')
+    title = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Assignment(models.Model):
+    name = models.TextField()
+    statement = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='assignments')
+
+    def __str__(self):
+        return self.name
+
+class Submission(models.Model):
+    student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='submissions')
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, related_name='submissions')
+    query = models.TextField()
+    feedback = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('student', 'assignment')
+
+    def __str__(self):
+        return f"{self.student} - {self.assignment}"
