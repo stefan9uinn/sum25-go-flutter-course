3ab515ad91653601c253e5a518136d9303c09e40
anna <annazyranova3074@gmail.com>
2025-06-28 21:13:42 +0300

    Some corrections

--- frontend/src/components/ExactClassroom/ExactClassroom.js
+++ frontend/src/components/ExactClassroom/ExactClassroom.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typography, Button } from "antd";
 import './ExactClassroom.css';
-import image from "../../img/Screen.jpg"
+import image from "../../img/WideScreen.jpg"
 
 const { Title, Paragraph, Text, Link } = Typography;
 
@@ -12,19 +12,22 @@ class ExactClassroom extends React.Component {
     
     return (
       <div className="classroom">
+        <img className="wide-image" src={image} style={{}}/>
         <Title style={{
           marginTop: 30,
-          color: "#51CB63",
+          color: "#fff",
           fontSize: 45,
           fontFamily: "'Noto Sans', sans-serif",
           fontWeight: 600,
           marginBottom: 10
-        }}>
-          {classroom.name}
-        </Title>
-        <Text>Primary Instructor: {classroom.teacher}</Text><br />
-        <Text>Number of Students: {classroom.numberOfStudents}</Text><br />
-        <Text>Created: {classroom.date}</Text>
+        }}>{classroom.name}</Title>
+
+        <div className="classroom-desciption">
+          <Text className="class-props">Primary Instructor: {classroom.teacher}</Text><br />
+          <Text className="class-props">Number of Students: {classroom.numberOfStudents}</Text><br />
+          <Text className="class-props">Created: {classroom.date}</Text>
+        </div>
+        
       </div>
     );
   }
