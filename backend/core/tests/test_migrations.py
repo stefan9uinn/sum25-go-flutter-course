f62b6dd1f742fd187cf121d8ecd59436a0c99d20
Stefan Farafonov <56768757+stefan9uinn@users.noreply.github.com>
2025-06-29 21:28:17 +0300

    Update test_migrations.py

--- backend/core/tests/test_migrations.py
+++ backend/core/tests/test_migrations.py
@@ -5,9 +5,5 @@ class MigrationsTest(TransactionTestCase):
     databases = {'default'}
 
     def test_apply_migrations(self):
-        """
-        Проверяем, что все миграции применяются без ошибок на чистой БД.
-        """
-        # Пробуем выполнить migrate на тестовой базе
         call_command("migrate", verbosity=0, interactive=False)
 
