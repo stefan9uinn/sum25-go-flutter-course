194b1bed7fb340fbcef82c71ff49c34d63f03933
stefan9uinn <stefanf1337@gmail.com>
2025-06-29 07:24:44 +0300

    replace none with None:)

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-if SECRET_KEY==none:
+if SECRET_KEY==None:
     SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-for-local")
 
 # SECURITY WARNING: don't run with debug turned on in production!
